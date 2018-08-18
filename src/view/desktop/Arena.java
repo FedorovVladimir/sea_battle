abbdeb84ba81294c4d2a30be998f00520c276c6c
user-penguin <kobzev96@gmail.com>
2018-08-18 16:40:14 +0700

    add arena constructor

--- src/view/desktop/Arena.java
+++ src/view/desktop/Arena.java
@@ -1,15 +1,53 @@
 package view.desktop;
 
+import battlefield.Cell;
 import game.Player;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Arena extends JFrame {
+
+    private JPanel window = new JPanel();
+    private JPanel playerField;
+    private JPanel heField;
 
     public Arena(Player player, Player he) {
         super("step of " + player.getName());
 
-        setSize(600, 600);
+        setSize(400, 810);
 
+        setResizable(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        playerField = initPlayerField(player);
+//        heField = initHeField(he);
+        window.add(playerField);
+
+        window.setLayout(new GridLayout(2,1));
+
+        add(window);
+
+    }
+
+    public JPanel initPlayerField(Player player) {
+        JPanel field = new JPanel();
+        field.setLayout(new GridLayout(10, 10));
+        field.setMaximumSize(new Dimension(400, 400));
+
+        Cell[][] justCells = player.getField().getMatrix();
+
+        CellButton[][] buttonCells = new CellButton[10][10];
+        for(int i = 0; i < 10; i++) {
+            for(int j = 0; j < 10; j++) {
+                buttonCells[i][j] = new CellButton(justCells[i][j].getCondition(), i, j);
+                field.add(buttonCells[i][j]);
+            }
+        }
+        return field;
+    }
+
+    public void updateWindow(Player player, Player he) {
+        window.
     }
 }
