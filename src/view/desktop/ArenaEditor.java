b054c0079d4acb9f7d075f397bf3e58ecaa05a84
FedorovVladimir <vladimir.fodorow@gmail.com>
2018-08-18 17:52:43 +0700

    add listener

--- src/view/desktop/ArenaEditor.java
+++ src/view/desktop/ArenaEditor.java
@@ -6,17 +6,40 @@ import game.Player;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 class ArenaEditor extends JFrame {
-
+    private JPanel jPanel = new JPanel();
     private FieldPanel fieldPanel;
+    private JPanel editor = new JPanel();
+
+    private JButton setShips = new JButton("Закончить расстановку");
 
     ArenaEditor(Player player) {
         super("Расставьте корабли, " + player.getName());
-        setSize(600, 600);
+        setSize(800, 420);
+        add(jPanel);
+        jPanel.setLayout(new GridLayout(1, 2));
 
         fieldPanel = new FieldPanel(player);
         fieldPanel.updateField(true);
-        add(fieldPanel);
+        jPanel.add(fieldPanel);
+
+        setShips.setVisible(true);
+        editor.add(setShips);
+
+        setShips.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                close();
+            }
+        });
+
+        jPanel.add(editor);
+    }
+
+    private void close() {
+        setVisible(false);
     }
 }
--- src/view/desktop/CellButton.java
+++ src/view/desktop/CellButton.java
@@ -3,6 +3,9 @@ package view.desktop;
 import battlefield.CellCondition;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
+import java.awt.*;
 
 class CellButton extends JButton {
     private CellCondition condition;
@@ -13,6 +16,8 @@ class CellButton extends JButton {
         this.condition = cellCondition;
         this.x = x;
         this.y = y;
+
+        setBorder(new LineBorder(Color.darkGray, 1));
     }
 
     CellCondition getCondition() {
